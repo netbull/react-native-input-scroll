24808acba6ea171b35459229816d6be78f4b8cdc
Aleksandar Dimitrov <netbull2007@gmail.com>
2019-08-19 09:47:23 +0300

    first commit

---
+++ ISSUE_TEMPLATE.md
@@ -0,0 +1,7 @@
+<!--
+
+In order to solve your problem quickly,
+
+Please use `https://snack.expo.io/` to make the smallest demo.
+
+-->
---
+++ __test__/App.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {StyleSheet, TextInput, View, Text} from 'react-native';
+import InputScrollView from './react-native-input-scroll-view';
+
+const MyComponent = props => (
+    <View>
+        <TextInput {...props} />
+    </View>
+)
+
+export default class App extends Component {
+  state = {
+    text: '',
+    textarea: '',
+  };
+
+  render() {
+    const { text, textarea } = this.state;
+    return (
+      <View style={styles.container}>
+        <InputScrollView>
+          <View style={styles.placeholder} />
+          <TextInput style={styles.input} value={text} />
+          <MyComponent style={styles.textarea}
+                     value={textarea}
+                     onChangeText={text => this.setState({ textarea: text })}
+                     multiline />
+        </InputScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#EEE',
+  },
+  placeholder: {
+    height: 400,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    margin: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+  },
+  textarea: {
+    margin: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+  },
+});
